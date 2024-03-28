666ff4b3f97fc16ce3f6dc5a9aa40ab6f8883622
Abay <161109060+AbirhamB@users.noreply.github.com>
2024-03-28 21:59:25 +0400

    Create Dict.py

---
+++ Dict.py
@@ -0,0 +1,11 @@
+Dict = {
+    "1":"one",
+    "2":"two",
+    "3":"three",
+    "4":"four",
+    "5":"five",
+    "6":"six"
+}
+change = input('Enter your number here: ')
+for i in change:
+    print(Dict[i], end=" ")
